b0ea0e46eb1770349028d661c173eb92864bb5d2
Talita Andrade Dos Anjos <talita.8880623@aluno.mg.gov.br>
2026-08-21 01:50:07 -0300

    feat: adicionar arrays de candidatos e matriz de votos

--- src/SistemaVotacao.java
+++ src/SistemaVotacao.java
@@ -8,6 +8,17 @@ public class SistemaVotacao {
     static final int TOTAL_TURMAS = 3;
     static final int MAX_VOTANTES_POR_TURMA = 10;
 
+    static int[] numerosCandidatos = new int[MAX_CANDIDATOS];
+    static String[] nomesCandidatos = new String[MAX_CANDIDATOS];
+    static int[] votosCandidatos = new int[MAX_CANDIDATOS];
+
+    static int[][] votosPorTurma =
+            new int[TOTAL_TURMAS][MAX_VOTANTES_POR_TURMA];
+
+    static int[] quantidadeVotosTurma = new int[TOTAL_TURMAS];
+
+    static int quantidadeCandidatos = 0;
+
     public static void main(String[] args) {
         System.out.println("Sistema de votação iniciado.");
         scanner.close();
