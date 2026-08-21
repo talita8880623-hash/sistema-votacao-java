c2414e1ef022cfbba194c3f321b5584741c1a643
Talita Andrade Dos Anjos <talita.8880623@aluno.mg.gov.br>
2026-08-21 01:02:08 -0300

    chore: criar estrutura inicial do programa

---
+++ src/SistemaVotacao/java/SistemaDeVotacao.java
@@ -0,0 +1,13 @@
+package SistemaVotacao.java;
+
+import java.util.Scanner;
+public class SistemaDeVotacao {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Sistema de votação iniciado.");
+
+        scanner.close();
+    }
+}
