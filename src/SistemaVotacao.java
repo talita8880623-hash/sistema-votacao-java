4c149ea0811cf6efc76eac8029a6ee824ab4bd92
Talita Andrade Dos Anjos <talita.8880623@aluno.mg.gov.br>
2026-08-21 01:36:46 -0300

    feat: definir limites de candidatos turmas e votantes

--- src/SistemaVotacao.java
+++ src/SistemaVotacao.java
@@ -1,13 +1,15 @@
-package SistemaVotacao.java;
-
 import java.util.Scanner;
-public class SistemaDeVotacao {
+
+public class SistemaVotacao {
 
     static Scanner scanner = new Scanner(System.in);
 
+    static final int MAX_CANDIDATOS = 5;
+    static final int TOTAL_TURMAS = 3;
+    static final int MAX_VOTANTES_POR_TURMA = 10;
+
     public static void main(String[] args) {
         System.out.println("Sistema de votação iniciado.");
-
         scanner.close();
     }
 }
