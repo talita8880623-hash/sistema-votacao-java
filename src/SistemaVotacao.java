d702f66c499a9f80bc362630c8597966d3b90f4f
Talita Andrade Dos Anjos <talita.8880623@aluno.mg.gov.br>
2026-08-21 01:53:08 -0300

    feat: implementar menu principal com do-while

--- src/SistemaVotacao.java
+++ src/SistemaVotacao.java
@@ -20,7 +20,47 @@ public class SistemaVotacao {
     static int quantidadeCandidatos = 0;
 
     public static void main(String[] args) {
-        System.out.println("Sistema de votação iniciado.");
+
+        int opcao;
+
+        do {
+            System.out.println("\n===== SISTEMA DE VOTAÇÃO =====");
+            System.out.println("1 - Cadastrar candidatos");
+            System.out.println("2 - Iniciar votação");
+            System.out.println("3 - Exibir resultado");
+            System.out.println("4 - Exibir matriz de votos");
+            System.out.println("5 - Sair");
+
+            System.out.print("Opção: ");
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Cadastro selecionado.");
+                    break;
+
+                case 2:
+                    System.out.println("Votação selecionada.");
+                    break;
+
+                case 3:
+                    System.out.println("Resultado selecionado.");
+                    break;
+
+                case 4:
+                    System.out.println("Matriz selecionada.");
+                    break;
+
+                case 5:
+                    System.out.println("Sistema encerrado.");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida.");
+            }
+
+        } while (opcao != 5);
+
         scanner.close();
     }
 }
